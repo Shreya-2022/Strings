5c951c81378bd5a9ac78eedf073cd5ca28e0724c
Shreya Tiwari <117257467+Shreya-2022@users.noreply.github.com>
2024-09-06 23:31:30 +0530

    + OPERATOR

---
+++ plusop.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    string str1 ="Shreya";
+    string str2 ="Tiwari";
+    str1+=str2;
+    cout<<str1<<endl;
+    return 0;
+}
