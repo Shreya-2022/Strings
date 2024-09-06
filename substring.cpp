49dda661ef75cb0824b5fa10c1625b1d6b659aa6
Shreya Tiwari <117257467+Shreya-2022@users.noreply.github.com>
2024-09-06 23:25:48 +0530

    Substring Funtion

---
+++ substring.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    string str = " Work pateintly";
+    // cout<<str.substr(pos.length);
+    cout<<str.substr(1,4)<<endl;
+    cout<<str.substr(1)<<endl;
+    return 0;
+}
