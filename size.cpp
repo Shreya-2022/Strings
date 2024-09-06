d3a04d359cbc5b23472779758279aa10ea6ac8df
Shreya Tiwari <117257467+Shreya-2022@users.noreply.github.com>
2024-09-06 23:41:44 +0530

    Size Function

---
+++ size.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    string str1 = "abcdef";
+    cout<<str1.size()<<endl;
+    char ch[20] ="abcdef";
+    cout<<(ch)<<endl;
+    return 0;
+}
