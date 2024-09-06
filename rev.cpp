22e3796017d43f9c8348d5e77c5fe73480824dcf
Shreya Tiwari <117257467+Shreya-2022@users.noreply.github.com>
2024-09-06 23:21:03 +0530

    Reverse funtion

---
+++ rev.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string>
+#include <algorithm>
+using namespace std;
+int main(){
+    string str = "Hello Shreya";
+   // reverse(str.begin(), str.end());
+    reverse(str.begin(), str.end()); // Reverse the string
+    cout<<str<<endl;
+    return 0;
+
+}
