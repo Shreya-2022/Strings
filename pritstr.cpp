778e9eb85db678ec8faa3c5bbed89729bc512670
Shreya Tiwari <117257467+Shreya-2022@users.noreply.github.com>
2024-09-06 22:29:37 +0530

    Print string

---
+++ pritstr.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<stdio.h>
+#include<string>
+using namespace std;
+ 
+int  main(){
+    string str ="College";
+    string str1 ="Wallah";
+    // string str1("wallah");
+    cout <<str<<" "<<str1<<endl;
+    cout <<str<<" "<<str1<<endl;
+    return 0;
+}
